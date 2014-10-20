78a1b18643b9bbb868c30b2931d9dd07c39ded38
Cristian Jara <cristian.prat.93@gmail.com>
2014-10-20 10:24:30 +0200

    Falling Geometry

---
+++ Assets/Scripts/QuadCtrl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadCtrl : MonoBehaviour {
+	
+	float lastrx;
+	int puntuacion;
+	public GUIText puntuacionGui;
+	// Use this for initialization
+	void Start () {
+		puntuacion = 0;
+		puntuacionGui.text = "Points: " + puntuacion.ToString();
+		//GameObject instance = Instantiate(Resources.Load("TVerde"), pos, Quaternion.identity) as GameObject;
+		InvokeRepeating("SpawnThings", 0.8f, 0.8f);//Invoca al spawneador cada z tiempo	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	void SpawnThings() {
+		float rx;
+		if(lastrx < 0) {
+			rx =  Random.Range(0f, 2f);
+		}
+		else {rx =  Random.Range(-2f, 0f);}
+		Vector3 pos = new Vector3(rx,6,-2);
+		float spawn =  Random.Range(0, 4);
+		
+		if (spawn == 0) {
+			GameObject instance = Instantiate(Resources.Load("TVerde"), pos, Quaternion.identity) as GameObject;
+		}
+		if (spawn == 1) {
+			GameObject instance = Instantiate(Resources.Load("TAmarillo"), pos, Quaternion.identity) as GameObject;
+		}
+		if (spawn == 2) {
+			GameObject instance = Instantiate(Resources.Load("TAzul"), pos, Quaternion.identity) as GameObject;
+		}
+		if (spawn == 3) {
+			GameObject instance = Instantiate(Resources.Load("TRojo"), pos, Quaternion.identity) as GameObject;
+		}	
+		lastrx = rx;
+	}
+}		
---
+++ Assets/Scripts/TverdeCtrl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TverdeCtrl : MonoBehaviour {
+	// Use this for initialization
+	float random = Random.Range (-10,10);
+	void Start () {
+		transform.rigidbody2D.AddTorque(random);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	 if(transform.position.y < -4f) {//Destruir en las y
+	 	Destroy(gameObject);
+	 }
+	 else if (transform.position.x > 3.5f || transform.position.x < -3.5f) {//Destruir en las x
+	 	Destroy(gameObject);
+	 }
+	}
+}
